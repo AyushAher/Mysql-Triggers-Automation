794fcc8238d79e5991564f63782b6cee7f5a7edf
Ayush Aher <ayushaher118@gmail.com>
2022-02-23 21:04:20 +0530

    Minor changes

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ user = input("User: ")
 password = input("Password: ")
 db = input("Database: ")
 
-# host = "localhost"
-# user = "root"
-# password = "ayushaher"
-# db = "avante"
 
 mydb = mysql.connector.connect(
     host=host,
@@ -19,7 +15,7 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor(buffered=True)
 path = input("Path of the file where you want to save: ")
-# path = "G:\Professional Projects\Avante\code\DB\Triggers"
+# path = "G:\Professional Projects\Zoho Extensions\Code\DB"
 actions = ["insert", "update", "delete"]
 
 
@@ -35,42 +31,44 @@ def init():
 def createTriggers(table, action, db):
     triggers = [
         f"\ncreate trigger {action}{table}trigger\n",
-        f" after {action} on {db}.{table} \n",
+        f" after {action} on `{db}`.{table} \n",
         "FOR EACH ROW \n",
-        "INSERT " + f"INTO {db}.audittrail\n",
+        "INSERT " + f"INTO `{db}`.audittrail\n",  # Replace audittrail with the databse table u want to save the information
         f' set action = "{action}",\n'
         'id = uuid(),\n',
         "createdon = sysdate(),\n",
         "updatedon = sysdate(),\n",
+        f"screen = '{table}',"
     ]
 
     mycursor.execute(f"show columns from {table}")
     lstColumns = mycursor.fetchall()
-    nvalue = "nvalue=concat('`{',"
-    ovalue = "ovalue=concat('`{',"
+    nvalue = "\nnvalue=concat('`{',"
+    ovalue = "\novalue=concat('`{',"
     for columns in lstColumns:
         columns = columns[0]
         nvalue += "'\"'" + f",'{columns}'," + "'\"'" + ",':'," + "'\"'" + f", new.{columns} ," + "'\",',"
         ovalue += "'\"'" + f",'{columns}'," + "'\"'" + ",':'," + "'\"'" + f", old.{columns} ," + "'\",',"
     nvalue = nvalue[:-1]
     nvalue = nvalue[:len(nvalue) - 5]
-    nvalue += "'\"'"
+    nvalue += ",'\"'"
     nvalue += ",'}`'),"
+
     ovalue = ovalue[:-1]
     ovalue = ovalue[:len(ovalue) - 5]
+    ovalue += ",'\"'"
     ovalue += ",'}`'),"
-    ovalue += "'\"'"
 
     if action == "insert":
         triggers.append(nvalue)
-        triggers.append("\nuserid = NEW.createdby")
+        triggers.append("\nuserid = NEW.createdby") # chheck wheather or not u want the userid or not
     elif action == "delete":
         triggers.append(ovalue)
-        triggers.append("\nuserid = old.createdby")
+        triggers.append("\nuserid = old.updatedby")
     else:
         triggers.append(ovalue)
         triggers.append(nvalue)
-        triggers.append("\nuserid = new.createdby")
+        triggers.append("\nuserid = new.updatedby")
 
     drop(table, action, db)
     triggers.append(";\n\n")
@@ -80,13 +78,14 @@ def createTriggers(table, action, db):
 
 
 def drop(table, action, db):
-    dTriggers = f"drop trigger {db}.{action}{table}trigger;\n"
+    dTriggers = f"drop trigger `{db}`.{action}{table}trigger;\n"
     file = open(f"{path}\Delete Triggers.sql", "a")
     file.writelines(dTriggers)
 
 
 if __name__ == '__main__':
     try:
+        print("Note: Triggers.sql and Delete Triggers.Sql files should be present before executing the code.")
         init()
     except Exception as e:
         print(e)
